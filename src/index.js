b76c574c74dc2b2ba0c7579c1ce19c8115c27a82
Tuuni <tuuni.mustonen@metropolia.fi>
2021-01-13 21:55:10 +0200

    valmis

--- src/index.js
+++ src/index.js
@@ -1,1 +1,130 @@
-console.log('Hello console!');
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+const timer = 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+  let userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = 'Aikaisemmat arvauksesi: ';
+  }
+  guesses.textContent += userGuess + ' ';
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = 'Jippii! Numero meni oikein!';
+    pause();
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = 'Hävisit Pelin!';
+    pause();
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Väärin!';
+    lastResult.style.backgroundColor = 'red';
+    if(userGuess < randomNumber) {
+      lowOrHi.textContent = 'Edellinen arvauksesi oli liian pieni luku';
+    } else if(userGuess > randomNumber) {
+      lowOrHi.textContent = 'Edellinen arvauksesi oli liian iso luku';
+    }
+  }
+
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+}
+
+guessSubmit.addEventListener('click', checkGuess);
+guessSubmit.addEventListener('click', start);
+
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Aloita uusi peli';
+  document.body.append(resetButton);
+  resetButton.addEventListener('click', resetGame);
+
+}
+
+function resetGame() {
+  guessCount = 1;
+  reset();
+  const resetParas = document.querySelectorAll('.resultParas p');
+  for (let i = 0 ; i < resetParas.length ; i++) {
+    resetParas[i].textContent = '';
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+
+  lastResult.style.backgroundColor = 'white';
+
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+
+//timer
+function timeToString(time) {
+  let diffInHrs = time / 3600000;
+  let hh = Math.floor(diffInHrs);
+
+  let diffInMin = (diffInHrs - hh) * 60;
+  let mm = Math.floor(diffInMin);
+
+  let diffInSec = (diffInMin - mm) * 60;
+  let ss = Math.floor(diffInSec);
+
+  let diffInMs = (diffInSec - ss) * 100;
+  let ms = Math.floor(diffInMs);
+
+  let formattedMM = mm.toString().padStart(2, "0");
+  let formattedSS = ss.toString().padStart(2, "0");
+  let formattedMS = ms.toString().padStart(2, "0");
+
+  return `${formattedMM}:${formattedSS}:${formattedMS}`;
+}
+
+let startTime;
+let elapsedTime = 0;
+let timerInterval;
+
+
+function print(txt) {
+  document.getElementById("display").innerHTML = txt;
+}
+
+
+
+function start() {
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(function printTime() {
+    elapsedTime = Date.now() - startTime;
+    print(timeToString(elapsedTime));
+  }, 10);
+}
+
+function pause() {
+  clearInterval(timerInterval);
+
+}
+
+function reset() {
+  clearInterval(timerInterval);
+  print("00:00:00");
+  elapsedTime = 0;
+}
